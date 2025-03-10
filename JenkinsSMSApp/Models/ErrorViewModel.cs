bb6e409b482ba2aa4c7b770aaf19ec81489408c4
Gahan Kotebagilu <Gahan.Kotebagilu@anthology.com>
2025-03-10 20:34:21 +0530

    Inital Commit

---
+++ JenkinsSMSApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JenkinsSMSApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
